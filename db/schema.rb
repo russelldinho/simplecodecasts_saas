7c6a665a604bf7d1a45e530ea827236116af706c
Tony <tony_russell77@hotmail.co.uk>
2018-04-30 20:00:35 +0000

    Created contact form view

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,11 @@
 ActiveRecord::Schema.define(version: 2018_04_30_185148) do
 
   create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
